593feeacd2cdbfc65edf6b2fe5b428289b6c9892
devjoshdev <jsinanan1@gmail.com>
2023-08-08 00:26:06 -0400

    Add some util functions for checking valid move, not done yet

--- components/TileBoard.js
+++ components/TileBoard.js
@@ -3,8 +3,36 @@ import Tile from "./Tile";
 import { useEffect, useState } from "react";
 
 const TileBoard = () => {
+    const findOpenPosition = (state) => {
+        for (let i = 0; i < state.length; i++) {
+            for (let j = 0; j < state[i].length; j++) {
+                if (state[i][j] === -1) {
+                    return [i, j];
+                }
+            }
+        }
+        return [-1, -1];
+    }
+    const isValidMove = (state, move) => {
+        switch (move) {
+            case "ArrowLeft":
+                const [x, y] = findOpenPosition(state);
+                if (y >= 0 && y < state[0].length - 1) {
+                    return true;
+                }
+                return false;
+            case "ArrowRight":
+                break;
+            case "ArrowUp":
+                break;
+            case "ArrowDown":
+                break;
+            default:
+                return false;
+        }
+    }    
     const handleKeyPress = (e) => {
-        console.log(e);
+        console.log('yuhh', isValidMove(board, e.key));
     };
     const [board, setBoard] = useState([
         [1, 2, 3, 4],
