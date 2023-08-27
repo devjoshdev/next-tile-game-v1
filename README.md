afb6b2578dcd2bbeffd553acc63c33e863ba4353
devjoshdev <jsinanan1@gmail.com>
2023-08-27 02:15:31 -0400

    Update README and add isGoalState and performMove functions, will need to refactor performMove since it doesn't function as it should

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ The object of the game is to use the one free space on the board to slide the ti
 
 **Notes**
 <hr/>
-- If using the metadata object for setting the title of a page, then to get your favicon to display properly you have to add the favicon as 'icon.ico' in the /app directory and add it to the metadata object like so:
 
+- If using the metadata object for setting the title of a page, then to get your favicon to display properly you have to add the favicon as 'icon.ico' in the /app directory and add it to the metadata object like so:
 ```javascript
 export const metadata = {
   title: "Tile Game",
@@ -21,4 +21,7 @@ export const metadata = {
   }
 }
 ```
-Source for this favicon is https://clipart-library.com/clipart/481588.htm
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Source for this favicon is https://clipart-library.com/clipart/481588.htm
+
+- React concept, adding an event listener for something like a keydown event is best done with useEffect, but *ALSO* remove the event listener on cleanup so that you don't reassign the handler numerous times *AND* the handler will be registered to act on proper state information
--- components/TileBoard.js
+++ components/TileBoard.js
@@ -3,6 +3,24 @@ import Tile from "./Tile";
 import { useEffect, useState } from "react";
 
 const TileBoard = () => {
+    const isGoalState = (state) => {
+        const numRows = state.length;
+        const numCols = state[0].length;
+        const elements = numRows * numCols - 1 // last element should be open (-1)
+        let counter = 1;
+        for (let i = 0; i < numRows; i++) {
+            for (let j = 0; j < numCols; j++) {
+                if (counter === elements) {
+                    return true;
+                }
+                if (state[i][j] !== counter) {
+                    return false;
+                }
+                counter++;
+            }
+        }
+        return false; // should not be hit
+    }
     const findOpenPosition = (state) => {
         for (let i = 0; i < state.length; i++) {
             for (let j = 0; j < state[i].length; j++) {
@@ -39,10 +57,50 @@ const TileBoard = () => {
             default:
                 return false; // if this line gets hit, we got some serious issues
         }
+    }
+    const performMove = (state, move) => {
+        const [xOpen, yOpen] = findOpenPosition(board);
+        let newBoard = [];
+        state.forEach(row => newBoard.push([...row]));
+        switch (move) {
+            case "ArrowLeft":
+                let firstElementOfRow = newBoard[xOpen][0];
+                for (let i =  0; i < newBoard[xOpen].length; i++) {   
+                    newBoard[xOpen][i] = newBoard[xOpen][i + 1];
+                }
+                newBoard[xOpen][newBoard[xOpen].length - 1] = firstElementOfRow;
+                setBoard(newBoard);
+                break;
+            case "ArrowRight":
+                let lastElementOfRow = newBoard[xOpen][newBoard[xOpen].length - 1];
+                for (let i = newBoard[xOpen].length - 1; i > 0; i--) {   
+                    // console.log("loking at index", i, "and elements", newBoard[xOpen][i], newBoard[xOpen][i-1]);
+                    newBoard[xOpen][i] = newBoard[xOpen][i - 1];
+                }
+                newBoard[xOpen][0] = lastElementOfRow;
+                setBoard(newBoard);
+                break;
+            case "ArrowUp":
+                let firstElementOfColumn = newBoard[0][yOpen];
+                for (let i = 0; i < newBoard.length - 1; i++) {
+                    newBoard[i][yOpen] = newBoard[i + 1][yOpen];
+                }
+                newBoard[newBoard.length - 1][yOpen] = firstElementOfColumn;
+                setBoard(newBoard);
+                break;
+            case "ArrowDown":
+                let lastElementOfColumn = newBoard[newBoard.length - 1][yOpen];
+                for (let i = newBoard.length - 1; i > 0; i--) {
+                    newBoard[i][yOpen] = newBoard[i - 1][yOpen]; 
+                }
+                newBoard[0][yOpen] = lastElementOfColumn;
+                setBoard(newBoard);
+                break;
+            default:
+                alert("Something went wrong!"); // if this line gets hit, we got some serious issues
+        }
     }    
-    const handleKeyPress = (e) => {
-        console.log('yuhh', isValidMove(board, e.key));
-    };
+
     const [board, setBoard] = useState([
         [1, 2, 3, 4],
         [5, 6, 7, 8],
@@ -50,9 +108,18 @@ const TileBoard = () => {
         [13, 14, 15, -1],
     ]);
     useEffect(() => {
-        window.addEventListener("keydown", handleKeyPress);
-    }, []);
-
+        const handleKeyPress = (e) => {
+            // console.log('yuhh', isValidMove(board, e.key));
+            // if the move is valid, make it knoiw
+            if (isValidMove(board, e.key)) {
+                performMove(board, e.key);
+            }
+        };
+        window.addEventListener("keydown", handleKeyPress); 
+        return () => window.removeEventListener("keydown", handleKeyPress)
+    }, [board]);
+    console.log("board", board);
+    console.log("is goal state?", isGoalState(board));
     return (
         <div style={{paddingLeft: "45%", paddingRight: "45%",}}>
             {board.map((row, rowIdx) => {
